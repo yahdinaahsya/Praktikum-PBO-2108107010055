3ec517510c331ade2cfd94436ae586102e59884e
Yahdina Ahsya <90942382+yahdinaahsya@users.noreply.github.com>
2022-09-06 10:17:53 +0700

    Commit Tugas 1

---
+++ praktikum 1/tugas.java
@@ -0,0 +1,17 @@
+package tugas;
+import java.util.Scanner;
+public class Tugas {
+    public static void main(String[] args) {
+      Scanner masuk = new Scanner(System.in);
+      System.out.print("Harga : ");
+      int harga = masuk.nextInt();
+      System.out.print("Jumlah barang : ");
+      int x = masuk.nextInt();
+      int jlh = x*harga;
+      int diskon = jlh * 15/100;
+      int hasil = harga-diskon;
+      
+      System.out.println("Harga setelah didiskon 15% adalah " +hasil); 
+    }
+    
+}
