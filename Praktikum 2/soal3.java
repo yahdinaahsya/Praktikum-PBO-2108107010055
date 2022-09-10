56568dc16d1f4b1f6aa62d8e025aff735dd1f4be
Yahdina Ahsya <90942382+yahdinaahsya@users.noreply.github.com>
2022-09-10 14:23:47 +0700

    Soal 3 Praktikum 2 selesai \^_^/

---
+++ Praktikum 2/soal3.java
@@ -0,0 +1,14 @@
+package Tugas;
+
+public class soal3 {
+    public static void main(String arg[]) {
+        //perulangan untuk banyak baris yang akan terbentuk
+        //tidak akan mencetak apa2 akan tetapi membuatnya menjadi baris baru
+        for (int i = 5; i > 0; i--) {
+            //perulangan untuk mencetak * dalam satu baris
+            for (int j = 0; j < i ; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+}
