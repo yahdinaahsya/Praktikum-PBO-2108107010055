ea66cb32dccacee2108ee74ef7df6a90cf5c8cdc
Yahdina Ahsya <90942382+yahdinaahsya@users.noreply.github.com>
2022-09-10 14:49:00 +0700

    menambahkan keterangan IP pada output

--- Praktikum 2/soal2.java
+++ Praktikum 2/soal2.java
@@ -5,25 +5,25 @@ public class soal2 {
         System.out.print("Masukkan nilai : ");
         int nilai = masuk.nextInt();
         if(nilai >= 87 && nilai <= 100){
-            System.out.println("Nilai huruf anda A");
+            System.out.println("Nilai huruf anda A, IP 4.00");
         }
         else if(nilai >= 78 && nilai<87){
-            System.out.println("Nilai huruf anda AB");
+            System.out.println("Nilai huruf anda AB, IP 3.50");
         }
         else if(nilai >= 69 && nilai<78){
-            System.out.println("Nilai huruf anda B");
+            System.out.println("Nilai huruf anda B, IP 3.00");
         }
         else if(nilai >= 60 && nilai<69){
-            System.out.println("Nilai huruf anda BC");
+            System.out.println("Nilai huruf anda BC, IP 2.50");
         }
         else if(nilai >= 51 && nilai<60){
-            System.out.println("Nilai huruf anda C");
+            System.out.println("Nilai huruf anda C, IP 2.00");
         }
         else if(nilai >= 41 && nilai<51){
-            System.out.println("Nilai huruf anda D");
+            System.out.println("Nilai huruf anda D, IP 1.00");
         }
         else if(nilai < 41){
-            System.out.println("Nilai huruf anda E");
+            System.out.println("Nilai huruf anda E, IP 0.00");
         }
         
         //untuk nilai diatas 100
