e6f1e2d8a34fdfcfea138ea20e4f90a8efd9a403
Yaya <90942382+yahdinaahsya@users.noreply.github.com>
2022-11-21 15:14:38 +0700

    Tugas 3-GUI
    done ^_^

---
+++ Praktikum 3/Tugas3/src/tugas3/Tugas3.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugas3;
+
+/**
+ *
+ * @author USER
+ */
+import javax.swing.*;
+public class Tugas3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame frame1 = new Nomor1();
+        frame1.setVisible(true);
+        JFrame frame2 = new Nomor2();
+        frame2.setVisible(true);
+    }
+    
+}
